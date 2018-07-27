29cbf688f008841607e3f5d2f9494198771a0494
MrOnepuntch <465080021@qq.com>
2018-07-27 23:21:03 +0800

    修改depoly

--- depoly.sh
+++ depoly.sh
@@ -1,15 +1,15 @@
-apt-get install git nginx
+apt-get -y install git nginx
+apt update
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install nodejs
+apt-get -y install nodejs
 npm install yarn -g
-yarn install
 yarn global add pm2
 
 rm -f /etc/nginx/sites-enabled/default
 rm -f /etc/nginx/sites-available/default
 
-ln -s -f /var/www/index/index.nginx /etc/nginx/sites-enabled/index
-chmod -R o+rwx /var/www/index
+ln -s -f /root/index/index.nginx /etc/nginx/sites-enabled/index
+# chmod -R o+rwx /var/www/index
 
 service nginx restart
 yarn run start
